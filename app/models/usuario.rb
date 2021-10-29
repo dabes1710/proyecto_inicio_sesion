65761c6ec6934ae439ff178acd035783f45948a7
dabes1710 <david.belmar@mayor.cl>
2021-10-29 17:30:04 -0300

    trabajo_1

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,5 +3,4 @@ class Usuario < ApplicationRecord
         validates :nombre, :apellido, presence: true, length: { in: 2..20 }
         validates :email, presence: true
         validates :edad, presence: true, numericality: { in: 10..150}
-      end
 end
--- db/migrate/20211029183707_create_usuarios.rb
+++ db/migrate/20211029183707_create_usuarios.rb
@@ -1,10 +1,10 @@
 class CreateUsuarios < ActiveRecord::Migration[6.1]
   def change
     create_table :usuarios do |t|
-      t.string :nombre=string
-      t.string :apellido=string
-      t.string :email=string
-      t.string :age=integer
+      t.string :nombre
+      t.string :apellido
+      t.string :email
+      t.integer :edad
 
       t.timestamps
     end
